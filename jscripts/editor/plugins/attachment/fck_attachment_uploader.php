19387fda9ae4e8076690a75442e799db6772daa8
wjames5 <will@tekimaki.com>
2008-05-24 23:20:16 +0000

    big overhaul of attachment uploader plugin - require an existing content, no more orphan attachments, and create a draft object when uploading - still plenty of js glitches - still need to update form with new content id if new one is created

--- jscripts/editor/plugins/attachment/fck_attachment_uploader.php
+++ jscripts/editor/plugins/attachment/fck_attachment_uploader.php
@@ -1,45 +1,101 @@
 <?php
 require_once( '../../../../../bit_setup_inc.php' );
-
-require_once(LIBERTY_PKG_PATH."LibertyAttachable.php");
 global $gBitSmarty, $gContent;
 
-$gContent = new LibertyAttachable();
-
-if (!empty($_REQUEST['content_id'])) {
-	$gContent->mContentId = $_REQUEST['content_id'];
-}
-
-if (isset($_FILES['upload'])) {
-	if (!$gContent->storeAttachments($_REQUEST, FALSE)) {
-		$gBitSmarty->assign('errors', $gContent->mErrors);
+$error = NULL;
+if ( !isset($_FILES['upload'] ) ) {
+	$error = "No upload submitted.";
+}elseif( !empty( $_REQUEST['content_id'] )) {
+// if we have a content id then we just load up that
+	if( !($gContent = LibertyBase::getLibertyObject( $_REQUEST['content_id'] )) ) {
+		// if there is something wrong with the content id spit back an error
+		$error = "You are attempting to upload a file to a content item that does not exist.";
 	}
-	elseif (empty($gContent->mContentId)) {
-		// Fake it for preflight
-		$gContent->mStorage[$_REQUEST['attachment_id']] = $gContent->getAttachment($_REQUEST['attachment_id']);		
+}elseif( isset ( $_REQUEST['content_type_guid'] ) ){
+/* if we don't have a content id then we assume this is new content and we need to create a draft.
+ * we'll pass a new content_id back to the edit form so it can make the right association later on save.
+ */
+	if( !isset( $gLibertySystem->mContentTypes[$_REQUEST['content_type_guid']] ) ){
+		$error = "You are attempting to upload a file to an invalid content type";
+	}else{
+		// load up the requested content type handler class
+		$contentType = $_REQUEST['content_type_guid'];
+		$contentTypeHash = $gLibertySystem->mContentTypes[$contentType];
+		$class =  $contentTypeHash['handler_class'];
+		$classFile =  $contentTypeHash['handler_file'];
+		$package = $contentTypeHash['handler_package'];
+		$pathVar = strtoupper($package).'_PKG_PATH';
+
+		if( !defined( $pathVar ) ) {
+			$error = "Undefined handler package path";
+		}else{
+			require_once( constant( $pathVar ).$classFile );
+			$gContent = new $class();
+		}
 	}
+}else{
+// if we don't have a valid content_id or content_type_guid we can't do nothing for you
+	$error = "You have not specified a content item or content type to associate this upload with";
 }
 
-// load the attachments up
-if (!empty($gContent->mContentId)) {
-	$gContent->load();
-}
-elseif (!empty($_REQUEST['existing_attachment_id'])) {
-	// Fake it for preflight
-	foreach( $_REQUEST['existing_attachment_id'] as $id) {
-		if (!empty($id)) {
-			$gContent->mStorage[$id] = $gContent->getAttachment($id);
+if( isset( $gContent ) ){
+	$storeHash = $_REQUEST;
+
+	if ( !$gContent->isValid() ){
+		// if we dont have a content object for this attachment yet, lets create a draft.
+		$storeHash['content_status_id'] = -5;
+	}else{
+		// else we'll skip storing the content
+		$storeHash['skip_content_store'] = true;
+	}
+
+	// store the attachment.
+	if ( !$gContent->store( $storeHash ) ) {
+		$error = $gContent->mErrors;
+		// If this is the first set it to be primary
+		if ( empty( $storeHash['STORAGE']['existing'] ) ) {
+			foreach ( $storeHash['STORAGE'] as $id => $file ) {
+				if ( $id != 'existing' ) {
+					foreach ( $file as $key => $data ) {
+						$gContent->setPrimaryAttachment( $key );
+						$done = true;
+						break;
+					}
+				}
+				if ( $done ) {
+					break;
+				}
+			}
+		}
+	}else{
+		// Load up the new attachment.
+		if ( !empty($storeHash['STORAGE'] ) ) {
+			foreach ( $storeHash['STORAGE'] as $id => $file ) {
+				if ( $id != 'existing' ) {
+					foreach ( $file as $key => $data ) {
+						$gContent->mStorage[$data['attachment_id']] = $gContent->getAttachment( $data['attachment_id'] );
+					}
+				}
+			}
 		}
 	}
 }
 
-// Make them come out in the right order
-ksort($gContent->mStorage);
+if ( !is_null( $error ) ){
+	$gBitSmarty->assign('errors', $error);
+}else{
+	// @Todo is this stuff necessary?
+	$gContent->load();
+	// Make them come out in the right order
+	if( !empty( $gContent->mStorage ) ) {
+		ksort( $gContent->mStorage );
+	}
+}
 
-// Make the actions work
-$gBitSmarty->assign('attachmentActionBaseURL', $_REQUEST['liberty_upload_action'].'?');
-$gBitSmarty->assign('gContent', $gContent);
-$gBitSmarty->assign('libertyUploader', true);
+$gBitSmarty->assign( 'gContent', $gContent );
+$gBitSmarty->assign( 'libertyUploader', TRUE );
+$gBitSmarty->assign( 'uploadTab', TRUE );
 
+// echo $gBitSystem->display( 'bitpackage:liberty/attachment_uploader.tpl', NULL, 'none' );
 echo $gBitSmarty->fetch( FCKEDITOR_PKG_PATH.'jscripts/editor/plugins/attachment/fck_attachment_uploader.tpl' );
 ?>
