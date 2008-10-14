b6557dff5262ef3f2521a2cc9fa7aa5519b14a9d
laetzer <laetzer@users.sourceforge.net>
2008-10-14 03:17:15 +0000

    updated version info; punctuation

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,10 +1,10 @@
 <?php
 global $gBitInstaller;
 $gBitInstaller->registerPackageInfo( FCKEDITOR_PKG_NAME, array(
-	'description' => "FCKEditor is a 'What You See Is What You Get' textarea HTML editor. It works with IE 5.5+ Firefox 1.0+ Mozilla 1.3+ and Netscape 7+. ",
+	'description' => "FCKEditor is a 'What You See Is What You Get' textarea HTML editor. It works with IE 5.5+, Firefox 1.0+, Mozilla 1.3+, and Netscape 7+. ",
 	'license' => '<a href="http://www.gnu.org/licenses/licenses.html#LGPL">LGPL</a>',
-	'important' => 'When using this WYSIWYG, we recommend that you either use HTML as the only content format or use TikiWiki with HTML allowed. (formats can be set in Administration --> Liberty --> Liberty plugins). It CAN NOT be used with TinyMCE active and is largely incompatible with QuickTags.',
-	'version' => '2.3.2',
+	'important' => 'When using this WYSIWYG editor, we recommend that you either use HTML as the only content format or use TikiWiki with the option <kbd>allow HTML</kbd> enabled (formats are set in Administration --> Liberty --> Liberty plugins). It <em>can not</em> be used with competing editor TinyMCE active, and it is largely incompatible with the package Quicktags.',
+	'version' => '2.6+',
 ) );
 
 $gBitInstaller->registerPreferences( FCKEDITOR_PKG_NAME, array(
